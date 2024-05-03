d682002e3d0e272c7f9a511443dce34692037e01
Deepak <ideepakrabindra@gmail.com>
2024-05-03 22:10:15 +0530

    - feat: Updated test script in package.json to use Jest - chore: Added tsconfig.json with compiler options for esModuleInterop

--- src/test/BasicUtils.spec.ts
+++ src/test/BasicUtils.spec.ts
@@ -3,6 +3,7 @@ import { product } from "../app/BasicUtils";
 describe("BasicUtils test suite", () => {
   it("return the product of 3 and 2", () => {
     const actual = product(3, 2);
+    expect(actual).toBe(6);
   });
 });
 
