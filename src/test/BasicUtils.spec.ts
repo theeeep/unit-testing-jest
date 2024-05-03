012898b6958984617907ed36939ae9516eceb2c2
Deepak <ideepakrabindra@gmail.com>
2024-05-03 22:35:58 +0530

    -  AAA Format Unit Test - Message: refactor(test): Improve test coverage for product function in BasicUtils.spec.ts

--- src/test/BasicUtils.spec.ts
+++ src/test/BasicUtils.spec.ts
@@ -1,9 +1,26 @@
 import { product } from "../app/BasicUtils";
 
+// describe("BasicUtils test suite", () => {
+//   it("return the product of 3 and 2", () => {
+//     const actual = product(3, 2);
+//     expect(actual).toBe(6);
+//   });
+// });
+
+//? AAA format
 describe("BasicUtils test suite", () => {
   it("return the product of 3 and 2", () => {
-    const actual = product(3, 2);
+    // arrange
+    const sut = product;
+
+    //acting
+    const actual = sut(3, 2);
+
+    // asserting
     expect(actual).toBe(6);
+    expect(actual).toEqual(6);
+    expect(actual).toBeLessThan(9);
+    expect(actual).toBeGreaterThan(5);
   });
 });
 
