f4d11b2bce4ee86cd073d645dacc46e7bf238759
Deepak <ideepakrabindra@gmail.com>
2024-05-03 23:34:17 +0530

    feat(test): add tests for user authentication in BasicUtils.spec.ts
    Added test cases for user authentication in the BasicUtils.spec.ts file to cover the functionality of authenticating users.

--- src/test/BasicUtils.spec.ts
+++ src/test/BasicUtils.spec.ts
@@ -24,6 +24,29 @@ describe("BasicUtils test suite", () => {
   });
 
   //? User Authenticate Test Suite
+  describe.only("Authenticate test suite", () => {
+    it("return the lower case of a valid user", () => {
+      const sut = authenticateUser;
+      const actual = sut("deveLOPER", "dev");
+      expect(actual.usernameToLower).toBe("developer");
+    });
+    it("return the username character of a valid user", () => {
+      const sut = authenticateUser;
+      const actual = sut("deveLOPER", "dev");
+      expect(actual.usernameCharacter).toEqual([
+        "d",
+        "e",
+        "v",
+        "e",
+        "L",
+        "O",
+        "P",
+        "E",
+        "R",
+      ]);
+    });
+  });
+
   it("User authenticate", () => {
     const sut = authenticateUser;
     const actual = sut("deveLOPER", "dev");
