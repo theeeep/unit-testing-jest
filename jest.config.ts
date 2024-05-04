9075a29aa3923f06e72b669a50e524180b42f68f
Deepak <ideepakrabindra@gmail.com>
2024-05-04 19:12:59 +0530

    feat: add code coverage configuration for Jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,8 @@ const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
   verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/src/app/**/*.ts"],
 };
 
 export default config;
